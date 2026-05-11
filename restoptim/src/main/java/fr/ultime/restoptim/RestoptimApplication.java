ff4b0c0c816982b1add6612181a4bd7c2f696c1b
paris <paris.albin23@gmail.com>
2026-05-11 11:10:50 +0200

    add backend project

---
+++ restoptim/src/main/java/fr/ultime/restoptim/RestoptimApplication.java
@@ -0,0 +1,13 @@
+package fr.ultime.restoptim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoptimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestoptimApplication.class, args);
+	}
+
+}
---
+++ restoptim/src/test/java/fr/ultime/restoptim/RestoptimApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ultime.restoptim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoptimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a01bd0467e4ba935993578181c7353a4847c69a9
roms-grlt <romaiongroultlgc@gmail.com>
2026-05-12 20:25:46 +0200

    moved api and config to new application package

--- restoptim/src/main/java/fr/ultime/restoptim/application/api/CuisineController.java
+++ restoptim/src/main/java/fr/ultime/restoptim/application/api/CuisineController.java
@@ -1,4 +1,4 @@
-package fr.ultime.restoptim.api;
+package fr.ultime.restoptim.application.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
